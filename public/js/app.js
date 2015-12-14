25d9f295641ff1a22d8a497d12adbd02b95cb661
Nick Brennan <nick_p_b@yahoo.com>
2015-12-14 14:39:26 -0800

    getting organized

---
+++ public/js/app.js
@@ -0,0 +1,17 @@
+$(function(){
+  var errorCallback = function(e) {
+    console.log('Reeeejected!', e);
+  };
+
+    // Not showing vendor prefixes.
+  navigator.webkitGetUserMedia({video: true, audio: false}, function(localMediaStream) {
+    var video = document.querySelector('video');
+    video.src = window.URL.createObjectURL(localMediaStream);
+
+    // Note: onloadedmetadata doesn't fire in Chrome when using it with getUserMedia.
+    // See crbug.com/110938.
+    video.onloadedmetadata = function(e) {
+      // Ready to go. Do some stuff.
+    };
+  }, errorCallback);
+});
